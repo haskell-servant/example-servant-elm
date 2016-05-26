4bf93f14d8eec8dca21421421e2b55fda9b37f08
Sönke Hahn <soenkehahn@gmail.com>
2016-05-26 15:53:49 +0800

    used elm-format

--- client/Main.elm
+++ client/Main.elm
@@ -1,106 +1,149 @@
+module Main exposing (..)
 
-import           Debug exposing (..)
-import           Dict exposing (..)
-import           Html exposing (..)
-import           Html.App exposing (program)
-import           Html.Attributes exposing (..)
-import           Html.Events exposing (..)
-import           Http
-import           Task exposing (Task, perform)
+import Debug exposing (..)
+import Dict exposing (..)
+import Html exposing (..)
+import Html.App exposing (program)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Task exposing (Task, perform)
+import Api exposing (..)
 
-import           Api exposing (..)
 
 main : Program Never
-main = program {
-    init = init,
-    update = update,
-    subscriptions = \ _ -> Sub.none,
-    view = view
-  }
+main =
+    program
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        , view = view
+        }
+
 
 type Either left right
-  = Left left
-  | Right right
+    = Left left
+    | Right right
 
-type alias State = {
-    items : Dict Int Item,
-    addItemInput : String,
-    error : Maybe String
-  }
 
-type alias ItemId = Int
+type alias State =
+    { items : Dict Int Item
+    , addItemInput : String
+    , error : Maybe String
+    }
+
+
+type alias ItemId =
+    Int
+
 
 type Message
-  = FromServer FromServer
-  | FromUi FromUi
+    = FromServer FromServer
+    | FromUi FromUi
+    | Error String
 
-  | Error String
 
 type FromServer
-  = Initial (List ItemId)
-  | NewItem Item
-  | Delete ItemId
+    = Initial (List ItemId)
+    | NewItem Item
+    | Delete ItemId
+
 
 type FromUi
-  = AddItemInputChange String
-  | AddItemButton
-  | Done ItemId
+    = AddItemInputChange String
+    | AddItemButton
+    | Done ItemId
 
-init : (State, Cmd Message)
+
+init : ( State, Cmd Message )
 init =
-  let
-    fetch = toServer Initial Api.getApiItem
-    state = {items = empty, addItemInput = "", error = Nothing}
-  in (state, fetch)
+    let
+        fetch =
+            toServer Initial Api.getApiItem
 
-update : Message -> State -> (State, Cmd Message)
-update message s = case message of
-  FromServer fromServer -> case fromServer of
-    Initial itemIds ->
-      let
-        cmd : Cmd Message
-        cmd = Cmd.batch <|
-          List.map (toServer NewItem << getApiItemByItemId) itemIds
-      in (s, cmd)
-    NewItem item -> noop {s | items = insert item.id item s.items}
-    Delete id -> noop {s | items = remove id s.items}
+        state =
+            { items = empty, addItemInput = "", error = Nothing }
+    in
+        ( state, fetch )
 
-  FromUi fromUi -> case fromUi of
-    AddItemButton ->
-      let
-        new = s.addItemInput
-        cmd = toServer (\ id -> NewItem (Item id new)) (postApiItem new)
-        newState = {s | addItemInput = ""}
-      in if new == ""
-        then update (Error "empty field") s
-        else (newState, cmd)
-    AddItemInputChange t -> noop {s | addItemInput = t}
-    Done id ->
-      let
-        cmd = toServer (always (Delete id)) (deleteApiItemByItemId id)
-      in (s, cmd)
 
-  Error msg -> ({s | error = Just msg}, Cmd.none)
+update : Message -> State -> ( State, Cmd Message )
+update message s =
+    case message of
+        FromServer fromServer ->
+            case fromServer of
+                Initial itemIds ->
+                    let
+                        cmd : Cmd Message
+                        cmd =
+                            Cmd.batch
+                                <| List.map (toServer NewItem << getApiItemByItemId) itemIds
+                    in
+                        ( s, cmd )
 
-noop : s -> (s, Cmd m)
-noop s = (s, Cmd.none)
+                NewItem item ->
+                    noop { s | items = insert item.id item s.items }
+
+                Delete id ->
+                    noop { s | items = remove id s.items }
+
+        FromUi fromUi ->
+            case fromUi of
+                AddItemButton ->
+                    let
+                        new =
+                            s.addItemInput
+
+                        cmd =
+                            toServer (\id -> NewItem (Item id new)) (postApiItem new)
+
+                        newState =
+                            { s | addItemInput = "" }
+                    in
+                        if new == "" then
+                            update (Error "empty field") s
+                        else
+                            ( newState, cmd )
+
+                AddItemInputChange t ->
+                    noop { s | addItemInput = t }
+
+                Done id ->
+                    let
+                        cmd =
+                            toServer (always (Delete id)) (deleteApiItemByItemId id)
+                    in
+                        ( s, cmd )
+
+        Error msg ->
+            ( { s | error = Just msg }, Cmd.none )
+
+
+noop : s -> ( s, Cmd m )
+noop s =
+    ( s, Cmd.none )
+
 
 toServer : (a -> FromServer) -> Task Http.Error a -> Cmd Message
-toServer tag task = perform (Error << toString) (FromServer << tag) task
+toServer tag task =
+    perform (Error << toString) (FromServer << tag) task
+
 
 view : State -> Html Message
 view state =
-  div [] <|
-    text (toString state) ::
-    br [] [] ::
-    (List.map (viewItem << snd) (toList state.items)) ++
-    input [onInput (FromUi << AddItemInputChange)] [] ::
-    button [onClick (FromUi AddItemButton)] [text "add item"] ::
-    []
+    div []
+        <| text (toString state)
+        :: br [] []
+        :: (List.map (viewItem << snd) (toList state.items))
+        ++ input [ onInput (FromUi << AddItemInputChange) ] []
+        :: button [ onClick (FromUi AddItemButton) ] [ text "add item" ]
+        :: []
+
 
 viewItem : Item -> Html Message
-viewItem item = div [] <|
-  text (item.text) ::
-  text " - " ::
-  button [onClick (FromUi <| Done item.id)] [text "done"] ::
-  []
+viewItem item =
+    div []
+        <| text (item.text)
+        :: text " - "
+        :: button [ onClick (FromUi <| Done item.id) ] [ text "done" ]
+        :: []
--- client/Tests.elm
+++ client/Tests.elm
@@ -1,26 +1,35 @@
-
 module Tests exposing (..)
 
 import ElmTest exposing (..)
-
 import Main exposing (..)
+
 
 main : Program Never
 main =
-  runSuite tests
+    runSuite tests
+
 
 tests : Test
 tests =
-  suite "client test suite" <|
-    addItemButton ::
-    []
+    suite "client test suite"
+        <| addItemButton
+        :: []
+
 
 addItemButton : Test
 addItemButton =
-  test "clean add item field on AddItemButton" <|
-    let
-      initState = fst init
-      s = {initState | addItemInput = "foo"}
-      (new, _) = update (FromUi AddItemButton) s
-      result = new.addItemInput
-    in assertEqual "" result
+    test "clean add item field on AddItemButton"
+        <| let
+            initState =
+                fst init
+
+            s =
+                { initState | addItemInput = "foo" }
+
+            ( new, _ ) =
+                update (FromUi AddItemButton) s
+
+            result =
+                new.addItemInput
+           in
+            assertEqual "" result
