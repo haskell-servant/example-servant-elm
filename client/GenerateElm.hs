afe3a0c4f5da74baea29e75a309937120b19d2e1
andys8 <andys8@users.noreply.github.com>
2019-08-04 22:24:31 +0200

    Format haskell files with brittany (0.12)

--- client/GenerateElm.hs
+++ client/GenerateElm.hs
@@ -3,20 +3,24 @@
 {-# LANGUAGE TypeOperators     #-}
 
 import           Data.List
-import           Data.Text hiding (intercalate, map)
-import           Servant.Elm  (DefineElm (DefineElm), Proxy (Proxy),
-                               defaultOptions, defElmImports, defElmOptions,
-                               deriveBoth, generateElmModuleWith)
+import           Data.Text                         hiding ( intercalate
+                                                          , map
+                                                          )
+import           Servant.Elm                              ( DefineElm(DefineElm)
+                                                          , Proxy(Proxy)
+                                                          , defaultOptions
+                                                          , defElmImports
+                                                          , defElmOptions
+                                                          , deriveBoth
+                                                          , generateElmModuleWith
+                                                          )
 import           Api
 
 main :: IO ()
-main =
-  generateElmModuleWith
-    defElmOptions
-    [ "Api" ]
-    defElmImports
-    "client"
-    [ DefineElm (Proxy :: Proxy Item)
-    , DefineElm (Proxy :: Proxy ItemId)
-    ]
-    (Proxy :: Proxy Api)
+main = generateElmModuleWith
+  defElmOptions
+  ["Api"]
+  defElmImports
+  "client"
+  [DefineElm (Proxy :: Proxy Item), DefineElm (Proxy :: Proxy ItemId)]
+  (Proxy :: Proxy Api)
--- server/test/AppSpec.hs
+++ server/test/AppSpec.hs
@@ -1,8 +1,13 @@
 module AppSpec where
 
-import           Control.Exception (throwIO, ErrorCall(..))
+import           Control.Exception                        ( throwIO
+                                                          , ErrorCall(..)
+                                                          )
 import           Control.Monad.Trans.Except
-import           Network.HTTP.Client (Manager, newManager, defaultManagerSettings)
+import           Network.HTTP.Client                      ( Manager
+                                                          , newManager
+                                                          , defaultManagerSettings
+                                                          )
 import           Network.HTTP.Types
 import           Network.Wai.Handler.Warp
 import           Servant.API
@@ -10,7 +15,7 @@ import           Servant.Client
 import           Test.Hspec
 
 import           Api
-import           App (app)
+import           App                                      ( app )
 
 getItemIds :: ClientM [ItemId]
 getItem :: ItemId -> ClientM Item
@@ -22,30 +27,31 @@ spec :: Spec
 spec = do
   describe "app" $ around withApp $ do
     context "/api/item" $ do
-      it "returns an empty list" $ \ host -> do
+      it "returns an empty list" $ \host -> do
         try host getItemIds `shouldReturn` []
 
       context "/api/item/:id" $ do
-        it "returns a 404 for missing items" $ \ (manager, baseUrl) -> do
-          Left err <- runClientM (getItem $ ItemId 23) (ClientEnv manager baseUrl Nothing)
+        it "returns a 404 for missing items" $ \(manager, baseUrl) -> do
+          Left err <- runClientM (getItem $ ItemId 23)
+                                 (ClientEnv manager baseUrl Nothing)
           errorStatus err `shouldBe` (Just notFound404)
 
       context "POST" $ do
-        it "allows to create an item" $ \ host -> do
+        it "allows to create an item" $ \host -> do
           i <- try host $ postItem "foo"
           try host (getItem i) `shouldReturn` Item i "foo"
 
-        it "lists created items" $ \ host -> do
+        it "lists created items" $ \host -> do
           i <- try host $ postItem "foo"
           try host getItemIds `shouldReturn` [i]
 
-        it "lists 2 created items" $ \ host -> do
+        it "lists 2 created items" $ \host -> do
           a <- try host $ postItem "foo"
           b <- try host $ postItem "bar"
           try host getItemIds `shouldReturn` [a, b]
 
       context "DELETE" $ do
-        it "allows to delete items" $ \ host -> do
+        it "allows to delete items" $ \host -> do
           i <- try host $ postItem "foo"
           _ <- try host $ deleteItem i
           try host getItemIds `shouldReturn` []
@@ -56,11 +62,11 @@ try :: Host -> ClientM a -> IO a
 try (manager, baseUrl) action = do
   result <- runClientM action (ClientEnv manager baseUrl Nothing)
   case result of
-    Right x -> return x
-    Left err -> throwIO $ ErrorCall $ show err
+    Right x   -> return x
+    Left  err -> throwIO $ ErrorCall $ show err
 
 withApp :: (Host -> IO a) -> IO a
-withApp action = testWithApplication app $ \ port -> do
+withApp action = testWithApplication app $ \port -> do
   manager <- newManager defaultManagerSettings
   let url = BaseUrl Http "localhost" port ""
   action (manager, url)
@@ -68,5 +74,5 @@ withApp action = testWithApplication app $ \ port -> do
 errorStatus :: ServantError -> Maybe Status
 errorStatus servantError = case servantError of
   FailureResponse response -> Just $ responseStatusCode response
-  _ -> Nothing
+  _                        -> Nothing
 
